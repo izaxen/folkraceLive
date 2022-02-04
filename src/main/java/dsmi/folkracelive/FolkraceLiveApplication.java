6fb352f881eddf621923fd5c43da0e9dd4872543
Martin Isaksen <izaxen@telia.com>
2022-02-04 10:59:24 +0100

    Initial commit

---
+++ src/main/java/dsmi/folkracelive/FolkraceLiveApplication.java
@@ -0,0 +1,13 @@
+package dsmi.folkracelive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkraceLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolkraceLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsmi/folkracelive/FolkraceLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dsmi.folkracelive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkraceLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
